086b3477d2a3eb3fd853026429d222bd0bceabb8
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-06 21:53:37 +0600

    Updates

--- src/main/java/project/steganography/HelloController.java
+++ src/main/java/project/steganography/HelloController.java
@@ -7,8 +7,10 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
