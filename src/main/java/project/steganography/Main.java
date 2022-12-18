b88cd1e4e5beee08c5191d66395b5d439bc622f3
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-18 19:54:40 +0600

    Updates

--- src/main/java/project/steganography/Main.java
+++ src/main/java/project/steganography/Main.java
@@ -1,8 +1,6 @@
 package project.steganography;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -12,24 +10,37 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button button = new Button();
-        button.setText("Run App");
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                // run the other JavaFX application
-                CipherApplication.main(null);
+        // Create a button to open the first app
+        Button app1Button = new Button("Open App 1");
+        app1Button.setOnAction(e -> openApp1());
+
+        // Create a button to open the second app
+        Button app2Button = new Button("Open App 2");
+        app2Button.setOnAction(e -> {
+            try {
+                new CipherApplication().start(primaryStage);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
             }
         });
 
+        // Add the buttons to a layout
         StackPane root = new StackPane();
-        root.getChildren().add(button);
+        root.getChildren().addAll(app1Button, app2Button);
 
+        // Set up the scene and stage
         Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Main Page");
+        primaryStage.setTitle("Main App");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void openApp1() {
+        // Code to open the first app goes here
+    }
+
+    private void openApp2() {
+        // Code to open the second app goes here
     }
 
     public static void main(String[] args) {
