88aa7e163c79116afb494a88f169deb21ba55d27
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-18 20:10:30 +0600

    Update Main.java

--- src/main/java/project/steganography/Main.java
+++ src/main/java/project/steganography/Main.java
@@ -3,7 +3,7 @@ package project.steganography;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,10 +12,20 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         // Create a button to open the first app
         Button app1Button = new Button("Open App 1");
-        app1Button.setOnAction(e -> openApp1());
+        app1Button.setLayoutY(50);
+        app1Button.setLayoutX(50);
+        app1Button.setOnAction(e -> {
+            try {
+                new LSB().start(primaryStage);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
 
         // Create a button to open the second app
         Button app2Button = new Button("Open App 2");
+        app2Button.setLayoutX(100);
+        app2Button.setLayoutY(100);
         app2Button.setOnAction(e -> {
             try {
                 new CipherApplication().start(primaryStage);
@@ -25,8 +35,9 @@ public class Main extends Application {
         });
 
         // Add the buttons to a layout
-        StackPane root = new StackPane();
-        root.getChildren().addAll(app1Button, app2Button);
+        Pane root = new Pane();
+        root.getChildren().addAll(app1Button);
+        root.getChildren().addAll(app2Button);
 
         // Set up the scene and stage
         Scene scene = new Scene(root, 300, 250);
@@ -35,13 +46,15 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    private void openApp1() {
-        // Code to open the first app goes here
-    }
-
-    private void openApp2() {
-        // Code to open the second app goes here
-    }
+//    private void openApp1() {
+//        // Code to open the first app goes here
+//
+//
+//    }
+//
+//    private void openApp2() {
+//        // Code to open the second app goes here
+//    }
 
     public static void main(String[] args) {
         launch(args);
