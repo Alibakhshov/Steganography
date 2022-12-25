1445da85617793b8cac7e72a24e18948466040da
Rauf Alibakhshov <91628590+Alibakhshov@users.noreply.github.com>
2022-12-25 08:18:22 +0600

    Updates

--- src/main/java/project/steganography/PictureSteganography/MenuLayout.java
+++ src/main/java/project/steganography/PictureSteganography/MenuLayout.java
@@ -11,17 +11,37 @@ public class MenuLayout {
 
   public static Pane layout(Stage window) {
     HBox layout = new HBox(5);
+    layout.setStyle(
+            "-fx-background-color: #f0f3f0;"
+            + "-fx-border-color: #f1dddd;"
+            + "-fx-border-width: 5;"
+            + "-fx-border-style: solid;"
+    );
     layout.setPrefSize(Steganography.WIDTH * 0.25, Steganography.HEIGHT * 0.1);
 
     Font font = new Font("Arial", Steganography.HEIGHT / 50);
 
     Button hideButton = new Button("Hide File");
+    hideButton.setStyle(
+            "-fx-background-color: #006c00;" +
+            "-fx-text-fill: white;" +
+            "-fx-font-size: 18px;" +
+            "-fx-font-weight: bold;" +
+            "-fx-cursor: hand;"
+    );
     hideButton.setFont(font);
     hideButton.setMinWidth(layout.getPrefWidth() / 2);
     hideButton.setMaxHeight(Double.MAX_VALUE);
     hideButton.setOnAction(e -> window.setScene(new Scene(HiderLayout.layout(window))));
 
     Button revealButton = new Button("Reveal File");
+    revealButton.setStyle(
+            "-fx-background-color: #006c00;" +
+                    "-fx-text-fill: white;" +
+                    "-fx-font-size: 18px;" +
+                    "-fx-font-weight: bold;" +
+                    "-fx-cursor: hand;"
+    );
     revealButton.setFont(font);
     revealButton.setMinWidth(layout.getPrefWidth() / 2);
     revealButton.setMaxHeight(Double.MAX_VALUE);
